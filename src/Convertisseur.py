e6176cccdbdfec34b0c086ba8f16340bad339886
Salahor <salahor20@gmail.com>
2023-11-14 15:23:48 +0100

    Initial

---
+++ src/Convertisseur.py
@@ -0,0 +1,11 @@
+def convertir(nombre):
+    if nombre <= 3:
+        return 'I'*nombre
+
+    if nombre == 4:
+        return 'IV'
+
+    if nombre <= 8:
+        return 'V' + 'I'*(nombre-5)
+
+    raise Exception('Erreur')
